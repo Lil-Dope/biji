ee65c1fc05ec7e14cd0d3d56c78e93924965a3b1
陈景涛 <wxchenjt@yutong.com>
2020-09-14 10:14:09 +0800

    202009

---
+++ auto-download.sh
@@ -0,0 +1,7 @@
+#/bin/nash
+
+cd /home/VersionManager/20200903/decidion/build
+
+wget --user yutong --password yutong123 http://10.66.226.116/repository/auto-drive/autodrive/ibus_decision/d/ibus_decision-d.1
+
+mv ibus_decision-d.1 ibus_decision-d  && chmod +x *
